fb41afe8a24a4e117093554fc11e21023aa53e7d
GabrielOlisil <gabriel.oliveira.s177@gmail.com>
2022-12-07 13:18:06 -0400

    Atualização de backup

--- React/imc/imc-react/src/App.tsx
+++ React/imc/imc-react/src/App.tsx
@@ -1,21 +1,88 @@
 import styles from './App.module.css'
 import Logo from './assets/powered.png'
+import React, { useState } from 'react';
+import styled from "styled-components";
 
 const App = () => {
+  const [heightField, setHeightField] = useState(0)
+  const [weightField, setWeightField] = useState(0);
+
+  const [positionMessagemErro, setPositionMessagemErro] = useState('errorMessageClosed');
+
   return (
+
     <div className={styles.main}>
+      <ErrorMessage positionMessagemError={positionMessagemErro}></ErrorMessage>
       <header>
         <div className={styles.headerContainer}>
-          <img src={Logo} alt="" width={150}/> 
+          <img src={Logo} alt="" width={150} />
         </div>
       </header>
       <div className={styles.container}>
-        <div className={styles.leftSide}>...</div>
-        <div className={styles.rightSide}>...</div>
+
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu imc</h1>
+          <p className={styles.description}>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização mundial de Saúde para calcular o peso ideal de cada pessoa</p>
+
+          <div className={styles.inputGrid}>
+
+            <input type="number" id="heightField"
+              onChange={(e) => setHeightField(+e.target.value)}
+              placeholder="Digite sua altura. Ex: 1,80"
+              value={heightField || ''} />
+
+            <input type="number" id="wheigthField"
+              onChange={(e) => setWeightField(+e.target.value)}
+              placeholder="Digite seu peso. Ex: 79"
+              value={weightField || ''} />
+
+            <button onClick={(e) => {
+              if (!(weightField && heightField)) {
+                setPositionMessagemErro('errorMessageOpened');
+                setTimeout(() =>{setPositionMessagemErro('errorMessageClosed')}, 2000);
+              }
+            }}>Calcular</button>
+          </div>
+        </div>
+
+
+        <div className={styles.rightSide}>
+          ...
+        </div>
 
       </div>
     </div>
   )
+}
+
+interface ErrorType {
+  positionMessagemError: string;
+}
+
+const ErrorMessage = ({positionMessagemError, ...props}: ErrorType) => {
+  const ErrorDiv = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 40px;
+    background-color: #ff6a6a;
+    transition: all ease .4s;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: transform 3s;
+    transform: translateY(-40px)
+  `;
+
+  const className = positionMessagemError;
+
+  return (
+      <ErrorDiv id='error' className={styles[className]}>
+        Erro, insira ambos os valores
+      </ErrorDiv>
+  );
 }
 
 export default App
---
+++ React/imc/imc-react/src/helpers/Imc.ts
@@ -0,0 +1,41 @@
+
+export interface Level {
+    title: string;
+    color: string;
+    icon: 'down' | 'up';
+    imc: number[];
+}
+export const levels: Level[] = [
+    {
+        title: "magreza",
+        color: '#96A3AB',
+        icon: 'down',
+        imc: [0, 18.5]
+    },
+    {
+        title: "normal",
+        color: '#0EAD69',
+        icon: 'up',
+        imc: [18.6, 24.9]
+    },
+    {
+        title: "sobrepeso",
+        color: '#E2B039',
+        icon: 'down',
+        imc: [25, 30]
+    },
+    {
+        title: "obesidade",
+        color: '#C3423F',
+        icon: 'down',
+        imc: [30.1, 99]
+    }
+];
+
+
+export const calcularImc = (height: number, weight: number) =>{
+    let imc = weight / (Math.pow(height, 2))
+
+    let level = levels.find((level) =>(imc >= level.imc[0] && imc <= level.imc[1]))
+    
+}
